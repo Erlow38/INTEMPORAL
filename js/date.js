375b8f2c59d034f1df99462f38ad0154f8c70248
Erlow <98266516+Erlow38@users.noreply.github.com>
2022-12-19 14:50:04 +0100

    Add files via upload

--- js/date.js
+++ js/date.js
@@ -1,7 +1,9 @@
 var today = new Date();
-var dd = today.getDate() + 1; // +1 to make it tomorrow
+var dd = today.getDate();
 var mm = today.getMonth() + 1; 
 var yyyy = today.getFullYear();
+var hour = today.getHours();
+var minute = today.getMinutes();
 
 if (dd < 10) {
    dd = '0' + dd;
@@ -11,7 +13,6 @@ if (mm < 10) {
    mm = '0' + mm;
 } 
     
-today = yyyy + '-' + mm + '-' + dd;
+today = yyyy + '-' + mm + '-' + dd + 'T' + hour + ':' + minute;
 year = yyyy;
-document.getElementById("date").setAttribute("min", today);
 document.getElementById("annee-circulation").setAttribute("max", year);
